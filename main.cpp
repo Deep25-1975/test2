22203e5fcd62d32db2b926ec5598175b86438564
Deep25-1975 <deep25.1975@gmail.com>
2024-11-07 22:15:37 +0530

    fixed error

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World 234!" << std::endl;
-    std::cout << "Hello, Roosh!" << std::endl;
+    std::cout << "Hello, Students!" << std::endl;
     return 0;
 }
